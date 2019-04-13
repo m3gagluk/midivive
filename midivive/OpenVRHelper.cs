2f8d6eac5748525b3676cb5ab86b2c1741d4a00a
m3gagluk <15657215+m3gagluk@users.noreply.github.com>
2019-04-13 19:29:07 +0300

    First release

---
+++ midivive/OpenVRHelper.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Valve.VR;
+
+namespace MidiTest
+{
+    class OpenVRHelper
+    {
+        private OpenVRHelper()
+        {
+
+        }
+        private static bool initialized = false;
+        public static bool InitHMD()
+        {
+            if (initialized)
+                return false; //Already initialized
+
+            EVRInitError error = EVRInitError.None;
+            CVRSystem vrSystem = OpenVR.Init(ref error, EVRApplicationType.VRApplication_Background);
+
+            if (error != EVRInitError.None)
+            {
+                Console.WriteLine("OpenVR init error: " + error);
+                return false;
+            }
+            //vrSystem.ResetSeatedZeroPose();
+
+            {
+                string filePath = string.Concat(Directory.GetCurrentDirectory(), "\\bindings\\actions.json");
+                EVRInputError err = OpenVR.Input.SetActionManifestPath(filePath);
+                if (err != EVRInputError.None)
+                {
+                    Console.WriteLine("OpenVR SetActionManifestPath error: " + err);
+                    return false;
+                }
+            }
+
+            handles = new ulong[2];
+            string[] sources = { "/user/hand/left", "/user/hand/right" };
+            for (int i = 0; i < 2; i++)
+            {
+                EVRInputError err = OpenVR.Input.GetInputSourceHandle(sources[i], ref handles[i]);
+                if (err != EVRInputError.None)
+                {
+                    Console.WriteLine("OpenVR GetInputSourceHandle error: " + err);
+                    return false;
+                }
+            }
+            vibrationHandle = 0;
+            {
+                EVRInputError err = OpenVR.Input.GetActionHandle("/actions/default/out/Haptic", ref vibrationHandle);
+                if (err != EVRInputError.None)
+                {
+                    Console.WriteLine("OpenVR GetActionHandle error: " + err);
+                    return false;
+                }
+            }
+            InitActionSet();
+            int periodMS = 5000;
+            Timer timer = new Timer(Refresh, null, 0, periodMS);
+            Refresh(null);
+            //wait for the timer to start
+            Thread.Sleep(periodMS);
+            initialized = true;
+            return true;
+        }
+
+        private static ulong[] handles;
+
+        private static ulong vibrationHandle = 0;
+
+        public static void PlayNote(int controller, float duration, float frequency, float volume)
+        {
+            if (!initialized)
+                return;
+            EVRInputError err = OpenVR.Input.TriggerHapticVibrationAction(vibrationHandle, 0, duration, frequency, volume, handles[controller]);
+            Refresh(null);
+            if (err != EVRInputError.None)
+            {
+                Console.WriteLine("OpenVR output error: " + err);
+                return;
+            }
+        }
+
+        private static VRActiveActionSet_t[] actionSet;
+
+        private static uint actionSetSize = Convert.ToUInt32(Marshal.SizeOf(typeof(VRActiveActionSet_t)));
+
+        private static void InitActionSet()
+        {
+            ulong handleLegacy = 0;
+            OpenVR.Input.GetActionSetHandle("/actions/default", ref handleLegacy);
+            actionSet = new VRActiveActionSet_t[1];
+            actionSet[0] = new VRActiveActionSet_t
+            {
+                ulActionSet = handleLegacy,
+                ulRestrictedToDevice = 0
+            };
+        }
+
+        private static void Refresh(object state)
+        {
+            EVRInputError err = OpenVR.Input.UpdateActionState(actionSet, actionSetSize);
+            if (err != EVRInputError.None)
+            {
+                Console.WriteLine("OpenVR UpdateActionState error: " + err);
+            }
+        }
+
+        public static void Shutdown()
+        {
+            OpenVR.Shutdown();
+        }
+    }
+}
---
+++ midivive/Program.cs
@@ -0,0 +1,204 @@
+﻿using NAudio.Midi;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace MidiTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (!OpenVRHelper.InitHMD())
+            {
+                return;
+            }
+            
+            //TODO: add args processing
+            MidiFile midi = new MidiFile("song.mid", false);
+            
+
+            Player[] players = new Player[2];
+            for (int i = 0; i < players.Count(); i++)
+            {
+                players[i] = new Player(i, false);
+            }
+
+
+            
+            double bpm = 120;
+
+            int divison = midi.DeltaTicksPerQuarterNote;
+
+
+            int[] trackEventIndex = new int[midi.Events.Count()];
+            long currentTick = 0;
+            Stopwatch sw = Stopwatch.StartNew();
+            double ticksPerQuarter = 1;
+            //based on https://github.com/ipatix/serialmidi/blob/master/serialmidi/Program.cs
+            while (true)
+            {
+                int tracksStopped = 0;
+                
+                for (int trackNum = 0; trackNum < midi.Events.Count(); trackNum++)
+                {
+                    var track = midi.Events[trackNum];
+                    if (trackEventIndex[trackNum] >= track.Count())
+                        continue;
+                    
+                    while (currentTick >= track[trackEventIndex[trackNum]].AbsoluteTime)
+                    {
+                        var ev = track[trackEventIndex[trackNum]];
+                        if (ev.CommandCode == MidiCommandCode.NoteOn)
+                        {
+                            NoteOnEvent note = (NoteOnEvent)ev;
+                            if(note.OffEvent == null)
+                            {
+                                Console.WriteLine("Note {0} doesn't have an off event, skipping.", note.NoteName);
+                            }
+                            else
+                            {
+                                //calculate note duration 
+                                double duration = note.NoteLength * ticksPerQuarter / 10000;
+
+                                //calculate frequency from the note number
+                                //https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+                                double frequency = (440F * Math.Pow(2, (note.NoteNumber - 69) / 12F));
+
+                                for (int i = 0; i < players.Count(); i++)
+                                {
+                                    if (players[i].IsBusy())
+                                        continue;
+                                    Console.WriteLine("Controller {0}: note {1} ({2:0.##} Hz) for {2:0.##} ms", i, note.NoteName, frequency, duration);
+                                    players[i].Play((float)duration, (float)frequency);
+                                    break;
+
+                                }
+                            }
+                            
+                        }
+                        if (ev.CommandCode == MidiCommandCode.MetaEvent)
+                        {
+                            MetaEvent meta = (MetaEvent)ev;
+                            if (meta.MetaEventType == MetaEventType.SetTempo)
+                            {
+                                TempoEvent tempoEvent = (TempoEvent)meta;
+                                bpm = tempoEvent.Tempo;
+                                Console.WriteLine("New tempo: "+ bpm + " BPM");
+                                //https://stackoverflow.com/questions/2038313/converting-midi-ticks-to-actual-playback-seconds
+                                ticksPerQuarter = (60 * Stopwatch.Frequency)/(bpm * divison);
+
+                            }
+                        }
+                        trackEventIndex[trackNum]++;
+                        if (trackEventIndex[trackNum] >= track.Count - 1)
+                        {
+                            tracksStopped++;
+                            Console.WriteLine("break " + midi.Events.Count());
+                            break;
+                        }
+
+                    }
+                }
+                //FIXME: playback noes not stop
+                if (tracksStopped == midi.Events.Count())
+                    break;
+                
+                currentTick++;
+
+                //just skip cycles for now
+                while(sw.ElapsedTicks < ticksPerQuarter)
+                {
+                    ;
+                }
+                sw.Restart();
+                
+            }
+            sw.Stop();
+            
+
+ 
+            //https://gitlab.com/Pilatomic/SteamControllerSinger/blob/master/main.cpp
+            
+            
+            Console.WriteLine("finished");
+            Console.ReadLine();
+
+            OpenVRHelper.Shutdown();
+        }
+
+        class Beep
+        {
+            private readonly float duration;
+            private readonly float frequency;
+
+            public Beep(float frequency, float duration)
+            {
+                this.duration = duration;
+                this.frequency = frequency;
+            }
+
+            public void Play()
+            {
+                new Thread(Play0).Start();
+            }
+
+            private void Play0()
+            {
+                var sineWaveProvider = new SignalGenerator
+                {
+                    Frequency = frequency
+                };
+                var waveOut = new WaveOut();
+                waveOut.Init(sineWaveProvider);
+                waveOut.Play();
+                Thread.Sleep((int)duration);
+                waveOut.Stop();
+            }
+        }
+        
+        class Player
+        {
+            private long started;
+            private float duration;
+            private readonly int controller;
+            private readonly bool debugMode;
+
+            public Player(int controller, bool debugMode)
+            {
+                this.controller = controller;
+                this.debugMode = debugMode;
+            }
+
+            private long GetUnixTime()
+            {
+                return ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds();
+            }
+
+            public bool IsBusy()
+            {
+                return GetUnixTime() - started < duration;
+            }
+
+            public void Play(float durationMS, float frequency)
+            {
+                started = GetUnixTime();
+                duration = durationMS;
+                if (debugMode)
+                {
+                    new Beep((int)frequency, (int)duration).Play();
+                }
+                
+                OpenVRHelper.PlayNote(this.controller, durationMS/1000F, frequency, 0.25F);
+            }
+        }
+        
+    }
+
+    
+}
